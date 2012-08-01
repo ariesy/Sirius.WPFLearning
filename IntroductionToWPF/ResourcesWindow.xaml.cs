1e5b4596379583dcebb6c918c8561d8f732faee6
Kun Zhu <ariesyzhk@gmail.com>
2012-08-01 17:56:46 +0800

    submit by kun zhu.

---
+++ IntroductionToWPF/ResourcesWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IntroductionToWPF
+{
+    /// <summary>
+    /// Interaction logic for ResourcesWindow.xaml
+    /// </summary>
+    public partial class ResourcesWindow : Window
+    {
+        public ResourcesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
