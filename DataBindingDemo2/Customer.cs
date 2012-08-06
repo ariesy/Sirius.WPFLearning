74a43de9f0569d2f3738abc2039f53beff86ccb6
Kun Zhu <ariesyzhk@gmail.com>
2012-08-06 17:00:33 +0800

    DataBindingDemo2

---
+++ DataBindingDemo2/Customer.cs
@@ -0,0 +1,22 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Customer.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace DataBindingDemo2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
