327fc727d9496c9ac47fcfbfe5e9606b92357d4e
Kun Zhu <ariesyzhk@gmail.com>
2012-08-06 16:51:51 +0800

    DataBindingDemo

---
+++ DataBindingDemo/BoolToBrushConverter .cs
@@ -0,0 +1,55 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="BoolToBrushConverter.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DataBindingDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(Brush))]
+    public class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            Brush b = null;
+
+            // Only apply the conversion if value is assigned and
+            // is of type bool.
+            if (value != null &&
+                value.GetType() == typeof(bool))
+            {
+                // true is painted with a green brush, 
+                // false with a red brush.
+                b = (bool)value ? Brushes.Green : Brushes.Red;
+            }
+
+            return b;
+        }
+
+        // Not used.
+        public object ConvertBack(
+            object value,
+            Type targetType,
+            object parameter,
+            CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
---
+++ DataBindingDemo/Student.cs
@@ -0,0 +1,40 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Student.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System.Collections.ObjectModel;
+
+namespace DataBindingDemo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Student
+    {
+        // The default constructor is required for creation from XAML.
+        public Student()
+        {
+        }
+
+        // The StudentName property is a string that holds the first and last name.
+        public string StudentName { get; set; }
+
+        // The IsEnrolled property gets or sets a value indicating whether
+        // the student is currently enrolled.
+        public bool IsEnrolled { get; set; }
+    }
+
+    // The StudentList collection is declared for convenience,
+    // because declaring generic types in XAML is inconvenient.
+    public class StudentList : ObservableCollection<Student>
+    {
+
+    }
+}
